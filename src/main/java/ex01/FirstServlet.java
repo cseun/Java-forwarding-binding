293127ee06cce2f15f92a451735812b96edd01fe
se eun <cseeun98@gmail.com>
2022-09-16 13:42:21 +0900

    new: Upload 자바웹을 다루는 기술 8.1강 - Redirect 실습
    패키지별로 기능을 나누어 실습
    ex01) Redirect
    ex02) Refresh
    ex03) location

---
+++ src/main/java/ex01/FirstServlet.java
@@ -0,0 +1,33 @@
+package ex01;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/first1")
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+	
+	protected void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+		
+		response.sendRedirect("second1");
+	}
+
+}
